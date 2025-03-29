51874395df7ee022d7dfa12ff859ef17287d16c6
eduff <122957991+eduff0@users.noreply.github.com>
2025-03-29 11:14:32 -0500

    Add packaging files to support pip install (setup.cfg, pyproject.toml)

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-# weewx_conditions_api/setup.py
-"""
-Installer for WeeWX-ConditionsAPI
-"""
+from setuptools import setup
 
-from setup import ExtensionInstaller
-
-def loader():
-    return ConditionsAPIInstaller()
-
-class ConditionsAPIInstaller(ExtensionInstaller):
-    def __init__(self):
-        super().__init__(
-            name='WeeWX-ConditionsAPI',
-            version='0.1',
-            description='Exposes RESTful API with current weather data from weewx.sdb',
-            author='Your Name',
-            author_email='your@email.com',
-            files=[
-                ('bin/user/weewx_conditions_api', [
-                    'bin/user/weewx_conditions_api/__init__.py',
-                    'bin/user/weewx_conditions_api/api_server.py',
-                ]),
-            ],
-            config={}
-        )
+setup()
