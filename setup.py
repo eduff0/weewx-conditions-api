40dcaf0f493572c8515d69c005f8f8f223406e23
eduff <122957991+eduff0@users.noreply.github.com>
2025-03-30 22:20:50 -0500

    Fix issue with isolated environment during install not allowing a check on weewx being installed

--- setup.py
+++ setup.py
@@ -1,14 +1,2 @@
-import sys
-
-# Pre-installation check: Ensure that weewx is already installed.
-try:
-    import weewx
-except ImportError:
-    sys.exit(
-        "Installation aborted: 'weewx' must be installed before installing weewx-conditions-api. "
-        "Please install weewx first."
-    )
-
 from setuptools import setup
-
 setup()
